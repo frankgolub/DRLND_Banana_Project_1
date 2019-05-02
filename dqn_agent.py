154b0732c90b31df5159f27b51979206b9f1e7e1
Frank Golub <frankgolub@gmail.com>
2019-05-02 10:24:18 -0400

    feat: Figures show results from hyperparameter search

--- dqn_agent.py
+++ dqn_agent.py
@@ -12,10 +12,11 @@ BUFFER_SIZE = int(1e5)  # replay buffer size
 BATCH_SIZE = 64         # minibatch size
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR = 5e-4               # learning rate 
+LR = 1e-3 # 5e-4               # learning rate 
 UPDATE_EVERY = 4        # how often to update the network
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print('Using device:', device)
 
 class Agent():
     """Interacts with and learns from the environment."""
